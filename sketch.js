93d35ee59baf7fc5dd59d527f263e5c434fba21c
Aridaman Nandan <aridaman1995@gmail.com>
2019-11-07 21:43:45 +0530

    added openSet and closedSet

--- sketch.js
+++ sketch.js
@@ -6,7 +6,16 @@ const CWIDTH = 400,
  */
 var rows = 5, 
     cols = 5,
+    w,
+    h,
     grid;
+/**
+ * A* algorithm related variables. 
+ */
+var openSet = [],
+    closedSet = [],
+    start,
+    end;
 
 function setup () {
     createCanvas(CWIDTH, CHEIGHT);
@@ -20,13 +29,64 @@ function setup () {
     /**
      * Initialize the grid. 
      */
+    w = width / cols;
+    h = height / rows;
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++)
-            grid[i][j] = new Spot();
+            grid[i][j] = new Spot(i, j);
     }
+    
+    /**
+     * Set start and end values. 
+     * Add initial value to open set. 
+     */
+    start = grid[0][0];
+    end = grid[cols - 1][rows - 1];
+    openSet.push (start);
 }
 
-
+/**
+ * This acts as while loop of the A* algorithm. 
+ */
 function draw () {
-    background(0)
-}
+    background(0);
+    /**
+     * While loop condition. 
+     */
+    if (openSet.length > 0) {
+        /**
+         * Find lowest f score in openset 
+         */
+        let winner = 0;
+        openSet.forEach((spot, index) => {
+            if (spot.f < openSet[winner].f)
+                winner = index;
+        });
+        /**
+         * If winner is the end then we have reached the solution. 
+         */
+        if (openSet[winner] ==  end) {
+            noLoop();
+            console.log("DONE!!!!")
+        }
+    } else {
+
+    }
+
+    /**
+     * Draw the grid. 
+     */
+    grid.forEach(col_elements => col_elements.forEach(element => element.show(color(255))));
+    /**
+     * Draw the open set. 
+     * - Green color. 
+     */
+    openSet.forEach(spot => spot.show(color(0, 255, 0)));
+    /**
+     * Draw the closed set. 
+     * - Red color. 
+     */
+    closedSet.forEach(spot => spot.show(color(255, 0, 0)));
+
+
+}   //  End of draw function. 
--- spot.js
+++ spot.js
@@ -2,9 +2,20 @@
  * Grid is made up of spots. 
  */
 class Spot {
-    constructor () {
+    constructor (i, j) {
+        this.x = i;
+        this.y = j;
         this.f = 0;
         this.g = 0;
         this.h = 0;
+    }
+
+    /**
+     * Show function. 
+     * Takes color as an argument. 
+     */
+    show (col) {
+        fill(col)        
+        rect (this.x * w, this.y * h, w - 1, h - 1);
     }
 }
